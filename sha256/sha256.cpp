fc2bf156a35b6a6ec64f9a375fb8ccda6d077284
byebunny <r.mamatkulova@yandex.com>
2024-11-14 21:01:57 +0300

    добавлены файлы

---
+++ sha256/sha256.cpp
@@ -0,0 +1,23 @@
+/**
+ * @file sha256.cpp
+ */
+
+#include "sha256.h"
+/**
+* @brief Получение хэш-кода по алгоритму md5
+* @param msg сообщения для хэширования
+* @return хэш-код
+*/
+
+std::string SHA256_hash(std::string msg)
+{
+    std::cout << "В США ХЭН ПРИШЛО: " << msg<<std::endl;
+    namespace CPP = CryptoPP;
+    
+    CPP::SHA256 hash;                        
+    CPP::byte digest[CryptoPP::SHA256::DIGESTSIZE];
+    hash.CalculateDigest(digest, (const CPP::byte*)msg.data(), msg.size());
+    std::string Hash;
+    CryptoPP::StringSource(digest, sizeof(digest), true, new CryptoPP::HexEncoder(new CryptoPP::StringSink(Hash)));
+    return Hash;
+}
---
+++ sha256/sha256.h
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+
+
+#include <cryptopp/hex.h>
+#include <cryptopp/osrng.h>
+
+
+
+std::string SHA256_hash(std::string msg);
